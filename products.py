43f9a4ea27d4f85cd5a46a0f27df1252fb4327de
Wayne Yang <f5v256@yahoo.com.tw>
2020-10-18 23:07:20 +0800

    add check file exit

--- products.py
+++ products.py
@@ -1,16 +1,22 @@
-#讀取檔案
-products = []
-with open('products.csv', 'r') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		s = line.strip().split(',')
-		name = s[0]
-		price = s[1]
-		products.append([name, price])
-print(products)
+import os
 
-#讓使用者輸入
+products = []#建立空清單
+#檢查檔案products有沒有在作業系統，如果有的話讀取檔案
+if os.path.isfile('products.csv'):
+    print('yeah! 找到檔案了')
+    with open('products.csv', 'r') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            s = line.strip().split(',')
+            name = s[0]
+            price = s[1]
+            products.append([name, price])
+    print(products)
+else:
+	print('找不到檔案............')
+
+#讓使用者新增購買記錄
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
